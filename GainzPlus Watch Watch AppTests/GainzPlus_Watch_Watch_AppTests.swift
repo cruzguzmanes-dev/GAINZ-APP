d55d6a1eaa6740ac05d6433f54bb4063bec69b3e
cruzguzmanes <cruzguzmans@gmail.com>
2026-03-16 23:53:39 -0600

    UnitTest Init

---
+++ GainzPlus Watch Watch AppTests/GainzPlus_Watch_Watch_AppTests.swift
@@ -0,0 +1,21 @@
+//
+//  GainzPlus_Watch_Watch_AppTests.swift
+//  GainzPlus Watch Watch AppTests
+//
+//  Created by Eric Sebastián on 16/03/26.
+//
+
+import Testing
+@testable import GainzPlus
+
+
+
+//struct GainzPlus_Watch_Watch_AppTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//        // Swift Testing Documentation
+//        // https://developer.apple.com/documentation/testing
+//    }
+//
+//}
---
+++ GainzPlus/Shared/Session/Session.swift
@@ -0,0 +1,7 @@
+//
+//  Session.swift
+//  GainzPlus
+//
+//  Created by Eric Sebastián on 16/03/26.
+//
+
